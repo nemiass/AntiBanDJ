91f4cfd5e54607f4510657bd7d1dc59f8c1c75c5
nemiias <nehemiastf@gmail.com>
2021-09-26 22:30:26 -0500

    join_channel, play, youtube_dl & ffmpeg

--- configs/config.py
+++ configs/config.py
@@ -1,5 +1,5 @@
 from os import path, sep
 
-TOKEN = ""
+TOKEN = "ODkxMTgxOTAyMDMwMDczODU3.YU6n0g.CjdfvyciwiImb5ZFyzzxy12GOg8"
 DEFAULT_PREFIX = "?"
 ROOT_PATH = path.dirname(path.abspath(__file__)).rsplit(sep, 1)[0]
