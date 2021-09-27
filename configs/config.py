d44bb246edc95d09e2a9610970ceb32b3165d6e0
nemiias <nehemiastf@gmail.com>
2021-09-26 22:46:16 -0500

    security changes

--- configs/config.py
+++ configs/config.py
@@ -1,5 +1,7 @@
 from os import path, sep
 
-TOKEN = "ODkxMTgxOTAyMDMwMDczODU3.YU6n0g.CjdfvyciwiImb5ZFyzzxy12GOg8"
 DEFAULT_PREFIX = "?"
 ROOT_PATH = path.dirname(path.abspath(__file__)).rsplit(sep, 1)[0]
+
+with open(f'{ROOT_PATH}/token.txt', 'r', encoding="utf-8") as f:
+    TOKEN = f.read()
